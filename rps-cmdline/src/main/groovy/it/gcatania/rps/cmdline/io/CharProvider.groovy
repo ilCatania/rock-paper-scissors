f3f62b23b7564c07a4a67aacf26c157d30b36b0a
Gabriele Catania <gabriele.ctn@gmail.com>
2014-11-13 01:25:28 +0100

    fix license header whitespace

--- rps-cmdline/src/main/groovy/it/gcatania/rps/cmdline/io/CharProvider.groovy
+++ rps-cmdline/src/main/groovy/it/gcatania/rps/cmdline/io/CharProvider.groovy
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package it.gcatania.rps.cmdline.io
 
 import groovy.util.logging.Slf4j
