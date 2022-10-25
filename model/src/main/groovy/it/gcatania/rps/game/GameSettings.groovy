27f8d67126c53b10e3adf966f8acb7fbf42299eb
Gabriele Catania <gabriele.ctn@gmail.com>
2022-10-25 21:04:56 +0100

    update license headers

--- model/src/main/groovy/it/gcatania/rps/game/GameSettings.groovy
+++ model/src/main/groovy/it/gcatania/rps/game/GameSettings.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Gabriele Catania <gabriele.ctn@gmail.com>
+ * Copyright 2022 Gabriele Catania <gabriele.ctn@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  *
  */
--- model/src/main/groovy/it/gcatania/rps/game/HandOutcome.groovy
+++ model/src/main/groovy/it/gcatania/rps/game/HandOutcome.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Gabriele Catania <gabriele.ctn@gmail.com>
+ * Copyright 2022 Gabriele Catania <gabriele.ctn@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
